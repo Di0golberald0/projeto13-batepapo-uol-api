996c2a25c865235e07ab4b1cc01f58311058345c
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-11-13 17:54:25 -0300

    participantSchema Done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 import joi from 'joi';
+import dayjs from 'dayjs';
 
 const app = express();
 
@@ -12,11 +13,25 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
+const participantSchema = joi.object({
+    name: joi.string().required()
+});
+
+const messageSchema = joi.object({
+
+});
+
 try {
     await mongoClient.connect();
     db = mongoClient.db('batepapo');
 } catch (err) {
     console.log(err);
 }
+
+app.post("/participants", (req, res) => {
+    const participant = req.body;
+
+
+});
 
 app.listen(process.env.PORT, () => console.log(`Server running in port: ${process.env.PORT}`));
